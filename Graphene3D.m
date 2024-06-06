de3fba5a842c985566694ca36342a94f369a6dd2
Mirza Akbar Ail <mirza.akbarali.59@gmail.com>
2024-06-06 17:21:52 +0200

    ds
    fd

--- Graphene3D.m
+++ Graphene3D.m
@@ -7,8 +7,8 @@ clc
 a_real = 2.46;
 t = -3.033;
 s=0.129;
-x=linspace(-3*pi/(sqrt(3)*a_real) ,3*pi/(sqrt(3)*a_real),1000);
-y=linspace(-3.5*pi/(2*a_real),5*pi/(3*a_real),1000);
+x=linspace(-3*pi/(sqrt(3)*a_real) ,3*pi/(sqrt(3)*a_real),2000);
+y=linspace(-3.5*pi/(2*a_real),5*pi/(3*a_real),2000);
 [a,b]=meshgrid(x,y);
 E_bonding= t*sqrt(1+4*cos(a*a_real*sqrt(3)/2).*cos(b*a_real/2)...
     +4*(cos(b*a_real/2)).^2)./(1+s*sqrt(1+4*...
@@ -23,7 +23,7 @@ E_antibonding=-t*sqrt(1+4*cos(a*a_real*sqrt(3)/2)...
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
 mesh(a,b,E_bonding)
-title('Energy dispersion relation of graphene','FontSize',20,'FontWeight','bold','Interpreter','latex')
+%title('Energy dispersion relation of graphene','FontSize',20,'FontWeight','bold','Interpreter','latex')
 xlabel('$k_{x}$','FontSize',20,'FontWeight','bold','Interpreter','latex')
 ylabel('$k_{y}$','FontSize',20,'FontWeight','bold','Interpreter','latex')
 zlabel('Energy (eV)','FontSize',20,'FontWeight','bold','Interpreter','latex')
